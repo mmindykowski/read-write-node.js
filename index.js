e84f50be6626f3bdebf577b95a18fd2693488571
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-10-02 21:56:20 +0200

    odczytanie pliku - zapis binarny

--- index.js
+++ index.js
@@ -8,9 +8,19 @@ const path = require("path");
 // metoda readdir
 
 fs.readdir(path.join(__dirname, "data"), function (err, files) {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(files);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(files);
+
+    files.forEach(function (file) {
+      fs.readFile(path.join(__dirname, "data", file), function (err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(data);
+        }
+      });
+    });
+  }
 });
