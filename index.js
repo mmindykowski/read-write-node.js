1e437763d53ddaf1dac94659446185c840af72c2
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-10-02 21:51:16 +0200

    przeniesienie i tablica z nazwami plików

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // modyfikacje plików
 
-const fs = reqire("fs");
+const fs = require("fs");
 
 // ścieżki do plików
 const path = require("path");
+
+// metoda readdir
+
+fs.readdir(path.join(__dirname, "data"), function (err, files) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(files);
+    }
+});
