66aa908b5084f064ee07a2c28590eaceeda7651f
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-10-02 22:08:17 +0200

    druga mozliwosc

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ fs.readdir(path.join(__dirname, "data"), function (err, files) {
     console.log(files);
 
     files.forEach(function (file) {
-      fs.readFile(path.join(__dirname, "data", file), function (err, data) {
+      fs.readFile(path.join(__dirname, "data", file), 'utf-8', function (err, data) {
         if (err) {
           console.log(err);
         } else {
-          console.log(data.toString());
+          console.log(data);
         }
       });
     });
