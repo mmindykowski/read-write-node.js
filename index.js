9db29abea565dc66e87ce1e326ce97b3811f5696
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-10-03 20:26:44 +0200

    ćwiczenie

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ fs.readdir(path.join(__dirname, "data"), function (err, files) {
   } else {
     console.log(files);
 
-    fs.mkdir(path.join(__dirname, "user"), function (err) {
+    fs.mkdir(path.join(__dirname, "users"), function (err) {
       if (err) {
         if (err.code === "EEXIST") {
           console.log("Folder już istnieje");
@@ -25,26 +25,34 @@ fs.readdir(path.join(__dirname, "data"), function (err, files) {
       }
     });
 
-    fs.writeFile(
-      path.join(__dirname, "user", "result.txt"),
-      "test",
-      function (err) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Stworzono plik");
-        }
-      }
-    );
+    files.forEach(function (file, index) {
+      fs.readFile(
+        path.join(__dirname, "data", file),
+        "utf-8",
+        function (err, data) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(JSON.parse(data));
+          }
 
-    // files.forEach(function (file) {
-    //   fs.readFile(path.join(__dirname, "data", file), 'utf-8', function (err, data) {
-    //     if (err) {
-    //       console.log(err);
-    //     } else {
-    //       console.log(JSON.parse(data));
-    //     }
-    //   });
-    // });
+          fs.writeFile(
+            path.join(
+              __dirname,
+              "users",
+              index + 1 + "-imie-nazwisko" + ".txt"
+            ),
+            "name surname street zip code city phone",
+            function (err) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("Stworzono plik");
+              }
+            }
+          );
+        }
+      );
+    });
   }
 });
