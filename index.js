ba9a876cfb213d1326efbbeadda415f7d8238ec8
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-10-03 19:59:09 +0200

    tworzenie folderu user

--- index.js
+++ index.js
@@ -13,14 +13,21 @@ fs.readdir(path.join(__dirname, "data"), function (err, files) {
   } else {
     console.log(files);
 
-    files.forEach(function (file) {
-      fs.readFile(path.join(__dirname, "data", file), 'utf-8', function (err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(JSON.parse(data));
-        }
-      });
+    fs.mkdir(path.join(__dirname, "user"), function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Stworzono folder");
+      }
     });
+    // files.forEach(function (file) {
+    //   fs.readFile(path.join(__dirname, "data", file), 'utf-8', function (err, data) {
+    //     if (err) {
+    //       console.log(err);
+    //     } else {
+    //       console.log(JSON.parse(data));
+    //     }
+    //   });
+    // });
   }
 });
